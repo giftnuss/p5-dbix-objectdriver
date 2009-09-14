099a5ee1f8bf48dcdfebb4fd6b21a6ce7851cf46
giftnuss <giftnuss@netscape.net>
2009-09-14 09:32:23 +0200

    this should be a flexible solution for helping setup record classes

---
+++ lib/DBIx/ObjectDriver/Record.pm
@@ -0,0 +1,19 @@
+package DBIx::ObjectDriver::Record;
+use strict; use warnings; use utf8;
+
+# =========================
+#          CLASS
+# =========================
+
+
+# =========================
+#         OBJECT
+# =========================
+use subs qw/init/;
+use HO::class
+    init => 'hash';
+
+1;
+
+__END__
+
